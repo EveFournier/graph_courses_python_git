3ab94a871595c293d4ad33aef6269d6c8895956e
EveFournier <evelyne.fournier@unige.ch>
2024-12-08 20:38:40 +0100

    set up

---
+++ Week3_4_fisrtquarto/test_cowsay.py
@@ -0,0 +1,12 @@
+print(2+2)
+
+
+# isolated environnement specific for this project to prevent version conflict
+import cowsay
+cowsay.cow("Hello all")
+#| eval: false
+#pip install plotly pandas jupyter ipykernel kaleido itables
+
+
+import cowsay
+cowsay.cow("Hello all")
